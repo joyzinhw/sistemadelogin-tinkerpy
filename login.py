bd970d407f7afb98c4bc12441ec88e9e937ddc13
joyzinhw <joycemouraifpi@gmail.com>
2023-04-24 16:32:46 -0300

    registro

--- login.py
+++ login.py
@@ -12,6 +12,7 @@ janela.configure(background="white")
 janela.resizable(width=False, height=False)
 janela.attributes("-alpha", 0.9)
 
+
 img_logo = PhotoImage(file="imagem/asa.png")
 
 #desing da janela e tamanho
@@ -19,31 +20,54 @@ img_logo = PhotoImage(file="imagem/asa.png")
 LeftFrame = Frame(janela, width=200, height=300, bg="blue", relief="raised")
 LeftFrame.pack(side=LEFT)
 
-RightFrame = Frame(janela, width=398, height=300, bg="black", relief="raised")
+RightFrame = Frame(janela, width=398, height=500, bg="black", relief="raised")
 RightFrame.pack(side=RIGHT)
-
-#inputs
 
 Logo_Label = Label(LeftFrame, image=img_logo,bg="blue")
 Logo_Label.place(x=50, y=100)
 
+#inputs
 User_Label = Label(RightFrame, text="Username", font=("Arial", 14), bg="black", fg="white")
-User_Label.place(x=7, y=100)
+User_Label.place(x=7, y=90)
 
 User_Entry = Entry(RightFrame, width=18)
-User_Entry.place(x=110, y=100)
+User_Entry.place(x=110, y=90)
 
 Pass_Entry = Label(RightFrame, text="Password", font=("Arial", 14), bg="black", fg="white")
-Pass_Entry.place(x=7, y=140)
+Pass_Entry.place(x=7, y=120)
 
-Pass_Entry = Entry(RightFrame, width=18)
-Pass_Entry.place(x=110, y=140)
+Pass_Entry = Entry(RightFrame, width=18, show="*")
+Pass_Entry.place(x=110, y=120)
 
 #button
 Login_Button = ttk.Button(RightFrame,text="login",width=16)
 Login_Button.place(x=10, y=250)
 
-Cad_Button = ttk.Button(RightFrame,text="cadastro",width=16)
+#remove os butoons
+def Cadastro():
+    Login_Button.place(x=5000)
+    Cad_Button.place(x=5000)
+    
+    Nome_Label = Label(RightFrame, text="Nome", font=("Arial", 14), bg="black", fg="white")
+    Nome_Label.place(x=7, y=30)
+    
+    Nome_Entry = Entry(RightFrame, width=18, justify=CENTER)
+    Nome_Entry.place(x=110, y=30)
+    
+    Email_Label = Label(RightFrame, text="E-mail", font=("Arial", 14), bg="black", fg="white")
+    Email_Label.place(x=7, y=60)
+    
+    Email_Entry = Entry(RightFrame, width=18, justify=CENTER)
+    Email_Entry.place(x=110, y=60)
+    
+    Cad= ttk.Button(RightFrame,text="Registrar",width=18)
+    Cad.place(x=230, y=250)
+    
+    Black = ttk.Button(RightFrame,text="Voltar",width=18)
+    Black.place(x=10, y=250)
+    
+    
+Cad_Button = ttk.Button(RightFrame,text="cadastro",width=16, command=Cadastro)
 Cad_Button.place(x=245, y=250)
 
 
